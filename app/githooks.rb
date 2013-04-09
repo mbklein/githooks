aeb806f700f807c47d3e100c22d10e3dc828e738
Michael Klein <mbklein@gmail.com>
2013-04-09 17:28:12 -0500

    Use ssh-agent for git interaction

--- app/githooks.rb
+++ app/githooks.rb
@@ -8,12 +8,12 @@ class GitHooks < Sinatra::Base
       head = payload['head_commit']
       cmds = [
         "cd #{settings.root}/tmp",
-        "git clone --depth 1 #{repo['url']} #{repo['name']}.#{$$}",
+        %{ssh-agent bash -c 'ssh-add $HOME/.ssh/id_github ; git clone --depth 1 git@github.com:#{repo['owner']['name']}/#{repo['name']} #{repo['name']}.#{$$}'},
         "cd #{repo['name']}.#{$$}",
-        "#{settings.root}/bin/git-shallow-submodule",
         "git checkout #{repo['id']}",
+        "#{settings.root}/bin/git-shallow-submodule",
         %{#{settings.root}/bin/git-flatten -m "#{head['message']}" flat},
-        "git push -f origin flat:flat",
+        %{ssh-agent bash -c 'ssh-add $HOME/.ssh/id_github ; git push -f origin flat:flat},
         "cd ..",
         "rm -rf #{repo['name']}.#{$$}"
       ]
